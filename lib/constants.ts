fb05cebb815ab0c4c3ef205d879174cd2c854596
silvarumk-ship-it <silvarumk@gmail.com>
2026-07-08 14:32:40 -0300

    Update constants.ts

---
+++ lib/constants.ts
@@ -0,0 +1,36 @@
+// lib/constants.ts
+
+// --------------------------
+// DADOS DA EMPRESA E CONTATO
+// --------------------------
+export const CONTATO = {
+  razaoSocial: "TAG TECNOLOGIA PARA O SISTEMA FINANCEIRO S.A.",
+  cnpj: "31.345.107/0001-03",
+  whatsapp: "5531996487583", // Formato correto para funcionar no link
+  telefone: "(31) 99648-7583",
+  email: "atendimento@tagtecnologia.com.br",
+  enderecoCompleto: `Alameda Xingu, 512, Edifício Evolution, 3º, 4º e 20º andares
+Bairro Alphaville Industrial
+Barueri - SP, CEP 06455-030`
+};
+
+// --------------------------
+// NOMES DAS IMAGENS (chaves únicas)
+// Salve os arquivos com esses nomes exatos dentro da pasta /public
+// --------------------------
+export const IMAGENS = {
+  logoPrincipal: "img-VEL-789452.png",
+  tagVeloe: "img-VEL-236791.png",
+  iconeWhatsapp: "ico-VEL-901456.png",
+  fundoPagina: "img-VEL-564823.jpg"
+};
+
+// --------------------------
+// LINKS DO SITE E ATENDIMENTO
+// --------------------------
+export const LINKS = {
+  politicaPrivacidade: "/politica-de-privacidade",
+  termosUso: "/termos-de-uso",
+  avisoCookies: "/aviso-de-cookies",
+  linkWhatsapp: `https://wa.me/${CONTATO.whatsapp}`
+};
