7bcf20c51c93d7f07e234fa98ef9a6799bbfb4d3
silvarumk-ship-it <silvarumk@gmail.com>
2026-07-08 15:22:29 -0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "TAG Tecnologia para o Sistema Financeiro",
+  description: "Soluções seguras, ágeis e transparentes para gestão de meios de pagamento e serviços financeiros.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
